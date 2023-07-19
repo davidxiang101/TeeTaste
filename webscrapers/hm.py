7d460a9e2e5a8ad2cf33ced4e6c3718b1406c560
davidxiang101 <davidxiang101@gmail.com>
2023-07-18 22:25:54 -0500

    hm working

---
+++ webscrapers/hm.py
@@ -0,0 +1,92 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import urllib
+
+# Set the desired user agent string
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"
+
+# Configure Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument(f"user-agent={user_agent}")
+
+# Set up the Chrome driver
+webdriver_service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+
+# This function will scrape images from the H&M fashion section
+def scrapeHMFashion(url, folderName, maxImages):
+    driver.get(url)
+    print("Folder Name:", folderName)
+
+    unique_images = set()  # Track unique image URLs
+    i = 1
+
+    while len(unique_images) < maxImages:
+        try:
+            # Find the images with class "item-image"
+            images = driver.find_elements(By.CSS_SELECTOR, ".item-image")
+
+            if not images:
+                break
+
+            for image in images:
+                if i > maxImages:
+                    break
+
+                # Get the image source URL
+                image_url = image.get_attribute("src")
+                if image_url and image_url not in unique_images:
+                    unique_images.add(image_url)
+                    print("Image number", i, ":\n", image_url, "\n")
+                    filename = f"image_{i}.jpg"
+
+                    # Create the folder if it doesn't exist
+                    if not os.path.exists(folderName):
+                        os.makedirs(folderName)
+
+                    # Download and save the image
+                    image_path = os.path.join(folderName, filename)
+                    urllib.request.urlretrieve(image_url, image_path)
+
+                    i += 1
+
+            # Scroll to load more images
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)  # Wait for the page to load new images
+
+            # Check if the "Load more products" button exists
+            load_more_button = driver.find_element(
+                By.CSS_SELECTOR, "button.js-load-more"
+            )
+            if load_more_button.is_displayed():
+                # Click the "Load more products" button
+                driver.execute_script("arguments[0].click();", load_more_button)
+                time.sleep(2)  # Wait for the page to load new products
+            else:
+                break
+
+        except Exception as e:
+            print("Error:", str(e))
+            break
+
+
+# Create a folder to store the images
+if not os.path.exists("images/HMImages"):
+    os.makedirs("images/HMImages")
+
+maxImages = 100
+
+url = "https://www2.hm.com/en_us/men/products/t-shirts-tank-tops.html"
+scrapeHMFashion(url, "images/HMImages", maxImages)
+
+# Close the browser
+driver.quit()
