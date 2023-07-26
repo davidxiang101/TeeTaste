02c5f21e1ae924e4649f2d3aba496b5894443767
davidxiang101 <davidxiang101@gmail.com>
2023-07-25 22:34:58 -0500

    t shirt fetch

--- tee-taste-frontend/components/tshirt.tsx
+++ tee-taste-frontend/components/tshirt.tsx
@@ -3,6 +3,15 @@
 import React, { useState } from 'react';
 
 const TShirtComponent = () => {
+    interface TShirt {
+        id: string;
+        imageUrl: string;
+    }
+
+    // Then use this type for your state variables
+    const [tshirt1, setTShirt1] = useState<TShirt | null>(null);
+    const [tshirt2, setTShirt2] = useState<TShirt | null>(null);
+
     const [loading, setLoading] = useState(false);
 
     const handleTShirtSelection = async (selectedId: string, notSelectedId: string) => {
@@ -33,6 +42,7 @@ const TShirtComponent = () => {
             }
 
             // Fetch new pair of T-shirts here
+            await fetchNextTShirts();
 
         } catch (error) {
             console.error('Error:', error);
@@ -41,41 +51,64 @@ const TShirtComponent = () => {
         }
     };
 
+    const fetchNextTShirts = async () => {
+        try {
+            const response = await fetch('/fetch_next_tshirts/'); // replace with your actual API endpoint
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            // assumes the API response is an object containing an array of two T-shirt objects
+            const newTShirtPair = data.tshirts;
+
+            setTShirt1(newTShirtPair[0]);
+            setTShirt2(newTShirtPair[1]);
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+
     return (
         <div className="flex min-h-screen bg-gray-100 justify-center items-center">
             {loading ? (
                 <div>Loading...</div>
-            ) : (
+            ) : tshirt1 && tshirt2 ? ( // make sure both T-shirts are loaded
                 <div className="flex space-x-8 shadow-lg p-8 rounded bg-white">
                     <div className="flex flex-col items-center">
                         <img
-                            src="/tshirt1.jpg"
-                            alt="T-Shirt 1"
+                            src={tshirt1.imageUrl} // Use T-shirt's imageUrl for src
+                            alt={`T-Shirt ${tshirt1.id}`} // Use T-shirt's id for alt
                             className="w-64 h-auto cursor-pointer"
-                            onClick={() => handleTShirtSelection('tshirt1', 'tshirt2')}
+                            onClick={() => handleTShirtSelection(tshirt1.id, tshirt2.id)} // Use T-shirt's ids for selection
                         />
                         <button
                             className="mt-4 py-2 px-4 rounded bg-blue-500 text-white hover:bg-blue-600 transition duration-200"
-                            onClick={() => handleTShirtSelection('tshirt1', 'tshirt2')}
+                            onClick={() => handleTShirtSelection(tshirt1.id, tshirt2.id)} // Use T-shirt's ids for selection
                         >
                             Select
                         </button>
                     </div>
                     <div className="flex flex-col items-center">
                         <img
-                            src="/tshirt2.jpg"
-                            alt="T-Shirt 2"
+                            src={tshirt2.imageUrl} // Use T-shirt's imageUrl for src
+                            alt={`T-Shirt ${tshirt2.id}`} // Use T-shirt's id for alt
                             className="w-64 h-auto cursor-pointer"
-                            onClick={() => handleTShirtSelection('tshirt2', 'tshirt1')}
+                            onClick={() => handleTShirtSelection(tshirt2.id, tshirt1.id)} // Use T-shirt's ids for selection
                         />
                         <button
                             className="mt-4 py-2 px-4 rounded bg-blue-500 text-white hover:bg-blue-600 transition duration-200"
-                            onClick={() => handleTShirtSelection('tshirt2', 'tshirt1')}
+                            onClick={() => handleTShirtSelection(tshirt2.id, tshirt1.id)} // Use T-shirt's ids for selection
                         >
                             Select
                         </button>
                     </div>
                 </div>
+            ) : (
+                <div>No T-Shirts found...</div>
             )}
         </div>
     );
