bf84aada586b156fb661c771177bf8103438dab2
davidxiang101 <davidxiang101@gmail.com>
2023-08-02 19:25:15 -0500

    scrape each page

--- teeTasteBackend/teeTasteBackend/webscrapers/stockx2.py
+++ teeTasteBackend/teeTasteBackend/webscrapers/stockx2.py
@@ -1,13 +1,12 @@
-import time
-import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-import urllib
+from selenium.webdriver.common.by import By
+import requests
+import os
+import time
+import random
 
-# Set the desired user agent string
+folderName = "images/StockXShoesImages"
 user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/95.0.1020.30 Safari/537.36",
@@ -19,88 +18,66 @@ user_agents = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/95.0",
 ]
 
+user_agent = random.choice(user_agents)
 headers = {
-    "User-Agent": user_agents[5],
+    "User-Agent": user_agent,
     "Accept-Language": "en-US,en;q=0.9",
 }
 
-# Configure Chrome options
-options = Options()
-options.add_argument("--headless")  # Run Chrome in headless mode (no GUI).
-options.add_argument("--disable-gpu")  # Disable GPU acceleration for headless mode.
+
+def scrape_stockx_shoe_images(url_base, num_pages):
+    options = Options()
+    options.add_argument("--disable-gpu")  # Disable GPU acceleration for headless mode.
+
+    driver = webdriver.Chrome(options=options)
+
+    # Track unique image URLs
+    unique_images = set()
+    image_urls = []
+
+    # Loop over the number of pages you want to scrape
+    for i in range(num_pages):
+        url = url_base.format(i + 1)
+        driver.get(url)
+
+        # Wait for the page to load and render the dynamic content (adjust the wait time as needed).
+        driver.implicitly_wait(10)
+
+        img_elements = driver.find_elements(
+            By.CSS_SELECTOR, "img.chakra-image.css-kpfxlo"
+        )
+
+        for img_element in img_elements:
+            image_url = img_element.get_attribute("src")
+            if image_url and image_url not in unique_images:
+                image_urls.append(image_url)
+                unique_images.add(image_url)
+
+    driver.quit()
+    return image_urls
 
 
-driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(10)
+if __name__ == "__main__":
+    stockx_url_base = "https://stockx.com/shoes?page={}"
+    num_pages = 10
+    image_urls = scrape_stockx_shoe_images(stockx_url_base, num_pages)
 
-
-# This function will scrape images from the StockX website
-def scrapeStockXShoes(driver, url, folderName, maxImages, timeout):
-    # Set up the Chrome driver
-    driver.get(url)
-    print("Folder Name:", folderName)
-
-    unique_images = set()  # Track unique image URLs
-    i = 1
-
-    start_time = time.time()
-    while len(unique_images) < maxImages:
+    for idx, image_url in enumerate(image_urls, start=1):
+        time.sleep(random.uniform(2.0, 5.5))  # Add a random delay between each download
+        idx = idx
         try:
-            # Find the images with CSS selector
-            images = driver.find_elements(
-                By.CSS_SELECTOR, "img.chakra-image.css-kpfxlo"
-            )
-
-            if not images:
-                break
-
-            for image in images:
-                if i > maxImages:
-                    break
-
-                # Get the image source URL
-                image_url = image.get_attribute("src")
-                if image_url and image_url not in unique_images:
-                    unique_images.add(image_url)
-                    print("Image number", i, ":\n", image_url, "\n")
-                    filename = f"image_{i}.jpg"
-
-                    # Create the folder if it doesn't exist
-                    if not os.path.exists(folderName):
-                        os.makedirs(folderName)
-
-                    # Download and save the image
-                    image_path = os.path.join(folderName, filename)
-                    urllib.request.urlretrieve(image_url, image_path)
-
-                    i += 1
-
-                if time.time() - start_time > timeout:
-                    print("Timeout reached. Exiting the loop.")
-                    break
-
-            if time.time() - start_time > timeout:
-                print("Timeout reached. Exiting the loop.")
-                break
-
-            next_button = driver.find_element(By.XPATH, '//a[@aria-label="Next"]')
-            next_button.click()
-            driver.implicitly_wait(10)
-
-        except Exception as e:
-            print("Error:", str(e))
-            break
-
-
-# Create a folder to store the images
-if not os.path.exists("images/StockXShoesImages"):
-    os.makedirs("images/StockXShoesImages")
-
-maxImages = 500
-timeout = 60  # Timeout in seconds
-
-url = "https://stockx.com/sneakers"  # Change the URL to the StockX sneakers section
-scrapeStockXShoes(driver, url, "images/StockXShoesImages", maxImages, timeout)
-
-# Close the browser
-driver.quit()
+            print(f"Image {idx}: {image_url}")
+            if not os.path.exists(folderName):
+                os.makedirs(folderName)
+            filename = "image_{}.jpg".format(idx)
+            # Download and save the image
+            image_path = os.path.join(folderName, filename)
+            img_response = requests.get(image_url, headers=headers)
+            if img_response.status_code == 200:
+                with open(image_path, "wb") as f:
+                    f.write(img_response.content)
+                print(f"Downloaded: {filename}")
+            else:
+                print(f"Failed to download image from URL: {image_url}")
+        except:
+            print("Failed to download image.")
