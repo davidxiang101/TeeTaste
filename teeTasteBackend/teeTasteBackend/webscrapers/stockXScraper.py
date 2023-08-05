048adc62358bb12fa365f3301c43962ce03564d3
Vincent Liu <vvinniev34@gmail.com>
2023-08-05 11:38:05 -0500

    more sneakers

--- teeTasteBackend/teeTasteBackend/webscrapers/stockXScraper.py
+++ teeTasteBackend/teeTasteBackend/webscrapers/stockXScraper.py
@@ -46,7 +46,7 @@ def download_images(url, download_path, output_file):
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         div_tags = soup.find_all('div', class_='css-tkc8ar')
 
-        i = 680
+        i = 800
         for div_tag in div_tags:
             img_url = div_tag.find('img').get('src')
             if img_url:
@@ -75,7 +75,7 @@ def download_images(url, download_path, output_file):
 
 if __name__ == "__main__":
     # ssense_url = "https://www.ssense.com/en-us/men/tops"
-    stockX = "https://stockx.com/sneakers?page=18"
+    stockX = "https://stockx.com/sneakers?page=21"
     download_path = "images/sneakers"  # The directory will be created if it doesn't exist
     output_file = "sneakers.txt"
     download_images(stockX, download_path, output_file)
