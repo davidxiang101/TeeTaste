57168dc027aed7af7fe01c40ba432d108a5c4438
davidxiang101 <davidxiang101@gmail.com>
2023-08-09 22:23:24 -0400

    loading update

--- tee-taste-frontend/components/loading.tsx
+++ tee-taste-frontend/components/loading.tsx
@@ -1,9 +1,10 @@
 const LoadingComponent = () => {
     return (
-        <div>
-            <p className="font-slate-100">Loading...</p>
+        <div className="flex flex-col items-center justify-center">
+            <div className="spinner w-9 h-9"></div>
+            <p className="font-slate-100 mt-2 text-gray-500">Loading...</p>
         </div>
-    )
-}
+    );
+};
 
 export default LoadingComponent;
