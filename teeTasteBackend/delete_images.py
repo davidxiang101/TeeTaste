edc6c268df90ea949d496802710759f89ba3260c
davidxiang101 <davidxiang101@gmail.com>
2023-08-10 17:05:40 -0400

    pyling update and reqs change

--- teeTasteBackend/delete_images.py
+++ teeTasteBackend/delete_images.py
@@ -1,9 +1,14 @@
+"""
+Module to delete all Shoe objects from the database.
+"""
+
 import os
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "teeTasteBackend.settings")
 django.setup()
 
+# pylint: disable=wrong-import-position, import-error
 from teeTasteBackend.models import Shoe
 
 # Delete all Shoe objects
