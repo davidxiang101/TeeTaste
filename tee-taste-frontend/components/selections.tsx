c2e5cdfef87606190f413143de4622a34369bd89
davidxiang101 <davidxiang101@gmail.com>
2023-08-13 19:31:05 -0400

    max selections

--- tee-taste-frontend/components/selections.tsx
+++ tee-taste-frontend/components/selections.tsx
@@ -11,16 +11,18 @@ interface PreviousSelectionsProps {
     selections: Shoe[];
 }
 
-const PreviousSelections: React.FC<PreviousSelectionsProps> = ({ selections }) => {
+const Selections: React.FC<PreviousSelectionsProps> = ({ selections }) => {
+    const limitedSelections = selections.slice(-6); // Only take the last six selections
+
     return (
-        <div className="flex flex-wrap justify-center mb-8">
-            {selections.map((shoe, index) => (
+        <div className="flex justify-center flex-nowrap overflow-x-hidden whitespace-nowrap mb-8">
+            {limitedSelections.map((selection, index) => (
                 <div key={index} className="m-4">
-                    <img src={shoe.fields.image} alt={`Previous Selection ${shoe.pk}`} className="w-32 h-auto" />
+                    <img src={selection.fields.image} alt={`Previous Selection ${selection.pk}`} className="w-32 h-auto" />
                 </div>
             ))}
         </div>
     );
 };
 
-export default PreviousSelections;
+export default Selections;
