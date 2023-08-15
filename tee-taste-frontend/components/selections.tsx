dbb6494cac7836d6b4cc9b7905d324f66b45b322
davidxiang101 <davidxiang101@gmail.com>
2023-08-14 21:31:27 -0400

    recs button

--- tee-taste-frontend/components/selections.tsx
+++ tee-taste-frontend/components/selections.tsx
@@ -9,18 +9,26 @@ interface Shoe {
 
 interface PreviousSelectionsProps {
     selections: Shoe[];
+    onGetRecommendations: () => void; // Define a callback for fetching recommendations
 }
 
-const Selections: React.FC<PreviousSelectionsProps> = ({ selections }) => {
+const Selections: React.FC<PreviousSelectionsProps> = ({ selections, onGetRecommendations }) => {
     const limitedSelections = selections.slice(-6); // Only take the last six selections
 
     return (
-        <div className="flex justify-center flex-nowrap overflow-x-hidden whitespace-nowrap mb-8">
-            {limitedSelections.map((selection, index) => (
-                <div key={index} className="m-4">
-                    <img src={selection.fields.image} alt={`Previous Selection ${selection.pk}`} className="w-32 h-auto" />
-                </div>
-            ))}
+        <div className="flex justify-center mb-8">
+            <div className="flex flex-nowrap overflow-x-hidden whitespace-nowrap">
+                {limitedSelections.map((selection, index) => (
+                    <div key={index} className="m-4">
+                        <img src={selection.fields.image} alt={`Previous Selection ${selection.pk}`} className="w-32 h-auto" />
+                    </div>
+                ))}
+            </div>
+            {limitedSelections.length === 6 && (
+                <button onClick={onGetRecommendations} className="ml-4 py-2 px-4 rounded bg-blue-700 text-zinc-200 hover:bg-blue-800 transition duration-200">
+                    Get Recommendations
+                </button>
+            )}
         </div>
     );
 };
